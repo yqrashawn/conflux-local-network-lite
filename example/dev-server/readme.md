0226ce6acf5353bd0a7ace133d65f721ff3aeb6c
yqrashawn <namy.19@gmail.com>
2020-07-15 18:26:43 +0800

    feat: change package name, add docs, fix dev server
    BREAKING CHANGE: change package name

---
+++ example/dev-server/readme.md
@@ -0,0 +1,3 @@
+# dev server example
+
+Start a dev server at http://localhost:12549 with a genblock button to generate block manually.
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {
   startGenBlock,
   stopGenBlock,
   genOneBlock,
-  sendCFX
+  sendCFX,
 } = require("./lib/chain.js");
 
 const devServer = require("./lib/dev-server.js");
@@ -15,27 +15,63 @@ const DEFAULT_PORT = 12539;
 
 let cfxNode;
 
+/**
+ * a fullnode controller with methods to start/quit/... a fullnode
+ *
+ * @class ConfluxNode
+ */
 class ConfluxNode {
-  constructor(opts) {
+  /**
+   * Creates an instance of ConfluxNode.
+   * @param {*} [opts={}] fullnode controller base options
+   * @param {Array} [opts.accounts=[]] a array of accounts to specify initial balance, [{ secretKey: "0xPrivateKey", balance: 1e23 }]
+   * @param {boolean} [opts.verbose=false] show more info?
+   * @param {number} [opts.genBlockInterval=300] the interval of automatically generating block
+   * @param {boolean} [opts.genBlockManually=false] won't automatically generate block if set to true, better use with opts.devServer
+   * @param {boolean} [opts.devServer=false] serve a page at http://localhost:12549 with a gen block button to manually generate block if set to true
+   * @param {number} [opts.devServer=12537] fullnode rpc http port
+   * @param {boolean} [opts.killPortProcess=false] kill other process occupying the port if set to true
+   * @memberof ConfluxNode
+   */
+  constructor(opts = {}) {
     this._parseOptions(opts);
   }
 
-  _parseOptions({
-    verbose = false,
-    genBlockInterval = DEFAULT_GEN_BLOCK_INTERVAL,
-    port = DEFAULT_PORT,
-    genBlockManually = false,
-    devServer = false,
-    killPortProcess = false
-  } = {}) {
-    this.verbose = verbose;
-    this.genBlockInterval = genBlockInterval;
-    this.killPortProcess = killPortProcess;
-    this.genBlockManually = genBlockManually;
-    this.devServer = devServer;
-    this.port = port;
+  _parseOptions(opts) {
+    if (!this.opts) this.opts = {};
+
+    Object.assign(
+      this.opts,
+      {
+        accounts: [],
+        verbose: false,
+        genBlockInterval: DEFAULT_GEN_BLOCK_INTERVAL,
+        port: DEFAULT_PORT,
+        genBlockManually: false,
+        devServer: false,
+        killPortProcess: false,
+      },
+      opts
+    );
   }
 
+  /**
+   * parse options from controller.start, fallback to base options, won't overwrite base options
+   *
+   * @param {*} opts
+   * @returns {Object} parsed options
+   * @memberof ConfluxNode
+   */
+  _parseStartOptions(opts) {
+    return Object.assign({}, this.opts, opts);
+  }
+
+  /**
+   * internal method to download the fullnode binary
+   *
+   * @returns {Promise<string>} the fullnode binary path on local machine
+   * @memberof ConfluxNode
+   */
   async _findBinary() {
     if (!this.bin) {
       this.bin = await getBinary();
@@ -46,71 +82,57 @@ class ConfluxNode {
     return this.bin;
   }
 
+  /**
+   * is fullnode running?
+   *
+   * @readonly
+   * @memberof ConfluxNode
+   */
   get running() {
     if (!cfxNode) return false;
     return !cfxNode.killed;
   }
 
-  _parseStartOptions({
-    verbose = false,
-    accounts = [],
-    genBlockInterval,
-    port,
-    genBlockManually,
-    killPortProcess
-  } = {}) {
-    return {
-      killPortProcess: killPortProcess || this.killPortProcess,
-      port: port || this.port,
-      verbose: verbose || this.verbose,
-      accounts,
-      genBlockInterval: genBlockInterval || this.genBlockInterval,
-      genBlockManually: genBlockManually || this.genBlockManually
-    };
-  }
-
-  async start(opts) {
+  /**
+   * start the fullnode
+   *
+   * @param {*} [opts={}] same options as the class constructor, opts will fallback to the base options passed into constructor, won't mutate the base opts
+   * @returns {Object ConfluxNode instance
+   * @memberof ConfluxNode
+   */
+  async start(opts = {}) {
     if (this.running) return;
 
     opts = this._parseStartOptions(opts);
 
-    const {
-      port,
-      verbose,
-      accounts,
-      genBlockInterval,
-      genBlockManually,
-      killPortProcess
-    } = opts;
+    const { port, accounts, genBlockManually } = opts;
 
     if (!(await this._findBinary())) return;
 
-    cfxNode = await start(this.bin, {
-      verbose,
-      port,
-      killPortProcess
-    });
+    cfxNode = await start(this.bin, opts);
 
     this.cfx = new Conflux({ url: `http://localhost:${port}` });
 
     if (!genBlockManually) {
-      startGenBlock(
-        genBlockInterval === undefined
-          ? this.genBlockInterval
-          : genBlockInterval
-      );
+      startGenBlock(opts);
     }
+
+    devServer.start({ ...opts, node: this });
 
     if (accounts) {
       await this.setupAccounts(accounts);
       await genOneBlock();
     }
 
-    devServer.start({ ...opts, node: this });
-
     return this;
   }
 
+  /**
+   * quit the fullnode and the dev server if there is one
+   *
+   * @returns {Object} ConfluxNode instance
+   * @memberof ConfluxNode
+   */
   async quit() {
     if (!this.running) return;
     stopGenBlock();
@@ -121,11 +143,24 @@ class ConfluxNode {
     return this;
   }
 
+  /**
+   * quit and start, accept same arguments with ConfluxNode.prototype.start
+   *
+   * @returns {Object} ConfluxNode instance
+   * @memberof ConfluxNode
+   */
   async restart() {
     await this.quit();
     return await this.start(...arguments);
   }
 
+  /**
+   * accept accounts same with the accounts in constructor options
+   *
+   * @param {Array[Object]} accounts a array of accounts to specify initial balance, [{ secretKey: "0xPrivateKey", balance: 1e23 }]
+   * @returns {Object} ConfluxNode instance
+   * @memberof ConfluxNode
+   */
   async setupAccounts(accounts) {
     if (!Array.isArray(accounts)) {
       throw new Error("accounts must be an array");
@@ -138,6 +173,13 @@ class ConfluxNode {
     return this;
   }
 
+  /**
+   * send some cfx from genesis account to the specified account
+   *
+   * @param {*} { address, balance, secretKey, privateKey }  eg. { secretKey: "0xPrivateKey", balance: 1e23 }
+   * @returns [Object] ConfluxNode instance
+   * @memberof ConfluxNode
+   */
   async sendCFX({ address, balance, secretKey, privateKey }) {
     if (secretKey) privateKey = secretKey;
     if (typeof balance !== "string") balance = `0x${balance.toString(16)}`;
@@ -145,15 +187,28 @@ class ConfluxNode {
     return this;
   }
 
+  /**
+   * generate one block, do nothing if fullnode not running
+   *
+   * @returns [Promise<Object>] Promise of ConfluxNode instance
+   * @memberof ConfluxNode
+   */
   async genOneBlock() {
+    if (!this.running) return;
     await genOneBlock();
     return this;
   }
 
-  changeGenBlockIntervalTo(interval = DEFAULT_GEN_BLOCK_INTERVAL) {
-    this.genBlockInterval = interval;
+  /**
+   * start or restart automatically generating block
+   *
+   * @param {number} [interval=DEFAULT_GEN_BLOCK_INTERVAL] interval to automatically generating block
+   * @returns {Object} ConfluxNode instance
+   * @memberof ConfluxNode
+   */
+  restartGenBlock(interval = DEFAULT_GEN_BLOCK_INTERVAL) {
     stopGenBlock();
-    startGenBlock(interval);
+    startGenBlock({ interval });
     return this;
   }
 }
