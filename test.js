869c90c9dd7fe2e782ade67ef546e1835c27036e
yqrashawn <namy.19@gmail.com>
2022-02-21 11:49:43 +0800

    feat: fullnode v2.0.0

--- test.js
+++ test.js
@@ -1,22 +1,23 @@
-const Local = require("./index.js");
+const Local = require("./index.js")
 const server = new Local({
+  verbose: true,
   genBlockInterval: 300,
-  killPortProcess: true
-});
+  killPortProcess: true,
+})
 server
   .start({
     accounts: [
       {
         secretKey:
           "0xA46F301E2D0AC3EE7B83303D93DD49C14CE8E18251EB623ED468AAA12F572E74",
-        balance: 1e23
-      }
-    ]
+        balance: 1e23,
+      },
+    ],
   })
   .then(() => {
-    process.exit();
+    process.exit()
   })
-  .catch(err => {
-    debugger;
-    throw err;
-  });
+  .catch((err) => {
+    debugger
+    throw err
+  })
